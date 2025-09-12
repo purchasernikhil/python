10304fa6d33af18c1c3dd5ba0a01e823352dd0a3
nikhil <anc@gmail.com>
2025-09-12 19:05:18 +0530

    all

--- files/01.variables.py
+++ files/01.variables.py
@@ -1,20 +1,21 @@
-# a = 2
-# b = 2
-# total = a + b
-# diff = a - b
-# mul = a * b
-# div = a / b
-# int_div = a // b
-# mod_div = a % b 
+a = 2
+b = 2
+total = a + b
+diff = a - b
+mul = a * b
+div = a / b
+int_div = a // b
+mod_div = a % b 
 
-# power = a ** b 
-# print(total)
-# print(diff)
-# print(mul)
-# print(div)
-# print(int_div)
-# print(mod_div)
-# print(power)
+power = a ** b 
+print(total)
+print(diff)
+print(mul)
+print(div)
+print(int_div)
+print(mod_div)
+print(power)
+---
 a = 10 
 print(a)
 a = a + 10
