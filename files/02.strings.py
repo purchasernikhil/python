33a8209b7155878a96ac70bdc42d2484b3911fda
nikhil <anc@gmail.com>
2025-09-15 18:13:24 +0530

    all

---
+++ files/02.strings.py
@@ -0,0 +1,23 @@
+# sample = 'a'
+# sample_str = "clearabc"
+# sample_str = """hyderabad and hyderabad, secunderabad are twin cities and i love living in hyderabad
+# My name is india 
+# i love my country
+# i love my USA
+# god bless america"""
+#print(sample_str.count("hyderabad"))
+#ans = sample_str.split(" ")
+# ans = sample_str.split("\n")
+# print(ans) 
+
+#print(sample_str.upper())
+#sample_str = "hyderabad and hyderabad, secunderabad are twin cities and i love living in hyderabad"
+# print(sample_str[-2])
+# print(sample_str[5])
+
+# print(sample_str[0:4])
+# print(sample_str[:4])  
+#print(sample_str[-2:])  
+
+sample_str = "hyderabad and hyderabad, secunderabad are twin cities and i love living in hyderabad"
+print(sample_str[1::2])
